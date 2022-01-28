c54e745b7dc5d7c2548dbdcc491259da6940ee08
Clucas0311 <clucas0311@gmail.com>
2022-01-28 18:52:51 -0500

    Added The Feedback component

--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -1,14 +1,29 @@
 import React, { useState } from "react";
 import Card from "./UI/Card";
 import Button from "./UI/Button";
+import RatingSelect from "./RatingSelect";
 
-const FeedbackForm = () => {
+const FeedbackForm = ({ handleAdd }) => {
   const [text, setText] = useState("");
+  const [rating, setRating] = useState("");
   const [btnDisabled, setBtnDisabled] = useState(true);
   const [message, setMessage] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        text: text,
+        rating: rating,
+      };
+
+      handleAdd(newFeedback);
+      setText("");
+    }
+  };
+
+  const onSelect = (user_rating) => {
+    setRating(user_rating);
   };
 
   const handleTextChange = (event) => {
@@ -28,8 +43,8 @@ const FeedbackForm = () => {
   return (
     <Card>
       <form onSubmit={handleSubmit}>
-        <h2>How woudl you rate your service with us?</h2>
-        {/* @todo - rating select component */}
+        <h2>How would you rate your service with us?</h2>
+        <RatingSelect select={onSelect} />
         <div className="input-group">
           <input
             type="text"
