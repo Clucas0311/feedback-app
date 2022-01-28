3d2f1ea7228a6594718895a2e2f67be199687c36
Clucas0311 <clucas0311@gmail.com>
2022-01-28 17:44:33 -0500

    Implemented Prop Drilling To Delete"

--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { FaTimes } from "react-icons/fa";
 import Card from "./UI/Card";
 import PropTypes from "prop-types";
 
-const FeedbackItem = ({ item }) => {
+const FeedbackItem = ({ item, onDelete }) => {
+  const handleClick = () => {
+    onDelete(item.id);
+  };
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
+      <button className="close" onClick={handleClick}>
+        <FaTimes color="purple" />
+      </button>
       <div className="text-display">{item.text}</div>
     </Card>
   );
